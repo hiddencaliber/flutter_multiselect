d88ae9cbb9a90c5079ff0bb4faaa9bf6df27385e
Randale Watson <rwatson.comsci@gmail.com>
2019-03-23 21:54:17 -0500

    Initial work on adding tests

--- test/flutter_multiselect_test.dart
+++ test/flutter_multiselect_test.dart
@@ -1,9 +1,159 @@
+import 'package:flutter_multiselect/selection_modal.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter/material.dart';
 
 import 'package:flutter_multiselect/flutter_multiselect.dart';
 
 void main() {
-  test('adds one to input values', () {
+  List<Map<String, dynamic>> dataSource = [];
 
+  setUp(() => {
+        dataSource = List<Map<String, dynamic>>()
+          ..addAll([
+            {
+              "display": "Australia",
+              "value": 1,
+            },
+            {
+              "display": "Canada",
+              "value": 2,
+            },
+            {
+              "display": "India",
+              "value": 3,
+            },
+            {
+              "display": "United States",
+              "value": 4,
+            }
+          ])
+      });
+
+  Widget getBasicMultiSelectApp(
+      MultiSelect multiSelect, NavigatorObserver observer, GlobalKey<FormState> formKey) {
+    return MaterialApp(
+      home: MyAppToTest(multiSelect, formKey),
+      navigatorObservers: [observer],
+    );
+  }
+
+  testWidgets('Clicking on MultiSelect loads selection modal',
+      (WidgetTester tester) async {
+    final MockNavigatorObserver mockNavigatorObserver = MockNavigatorObserver();
+    final multiSelect = MultiSelect(
+      dataSource: dataSource,
+      textField: 'display',
+      valueField: 'value',
+    );
+    var app = getBasicMultiSelectApp(multiSelect, mockNavigatorObserver, new GlobalKey<FormState>());
+    await tester.pumpWidget(app);
+    Finder multiSelectFinder = find.byType(MultiSelect);
+    expect(multiSelectFinder, findsOneWidget);
+    await tester.tap(multiSelectFinder);
+    await tester.pumpAndSettle();
+    verify(mockNavigatorObserver.didPush(any, any));
+    Finder selectionModalFinder = find.byType(SelectionModal);
+    expect(selectionModalFinder, findsOneWidget);
   });
+
+  testWidgets('Selection modal has all elements passed in',
+      (WidgetTester tester) async {
+    final MockNavigatorObserver mockNavigatorObserver = MockNavigatorObserver();
+    final multiSelect = MultiSelect(
+      dataSource: dataSource,
+      textField: 'display',
+      valueField: 'value',
+    );
+    var app = getBasicMultiSelectApp(multiSelect, mockNavigatorObserver, new GlobalKey<FormState>());
+    await tester.pumpWidget(app);
+    Finder multiSelectFinder = find.byType(MultiSelect);
+    await tester.tap(multiSelectFinder);
+    await tester.pumpAndSettle();
+    Finder selectionModalFinder = find.byType(SelectionModal);
+    Finder selectionOptionsFinder = find.descendant(
+        of: selectionModalFinder, matching: find.byType(ListTile));
+    expect(selectionOptionsFinder, findsNWidgets(dataSource.length));
+  });
+
+  testWidgets('Selection adds to selected option when option clicked',
+      (WidgetTester tester) async {
+    final MockNavigatorObserver mockNavigatorObserver = MockNavigatorObserver();
+    final multiSelect = MultiSelect(
+      dataSource: dataSource,
+      textField: 'display',
+      valueField: 'value',
+    );
+    var app = getBasicMultiSelectApp(multiSelect, mockNavigatorObserver, new GlobalKey<FormState>());
+    await tester.pumpWidget(app);
+    Finder multiSelectFinder = find.byType(MultiSelect);
+    await tester.tap(multiSelectFinder);
+    await tester.pumpAndSettle();
+    Finder selectionModalFinder = find.byType(SelectionModal);
+    Finder selectionOptionsFinder = find.descendant(
+        of: selectionModalFinder, matching: find.byType(ListTile));
+    await tester.tap(selectionOptionsFinder.first);
+    await tester.pump();
+    Finder optionsThatExistFinder = find.byType(Chip);
+    expect(optionsThatExistFinder, findsOneWidget);
+    Finder selectedOptionTextFinder = find.descendant(
+        of: optionsThatExistFinder, matching: find.byType(Text));
+    var selectedOptionText =
+        selectedOptionTextFinder.evaluate().single.widget as Text;
+    expect(dataSource.first[multiSelect.textField], selectedOptionText.data);
+  });
+
+  testWidgets('Selection has matching initialValuea passed in',
+      (WidgetTester tester) async {
+    final MockNavigatorObserver mockNavigatorObserver = MockNavigatorObserver();
+    final indcesToUseForInitalValues = [1, 3];
+    final initialMemebers  = List<Map<String, dynamic>>();
+    final formKey = new GlobalKey<FormState>();
+    for (var item in indcesToUseForInitalValues) {
+      initialMemebers.add(dataSource.elementAt(item));
+    }
+    final List<dynamic> initialValues = initialMemebers.map((d) => d['value']).toList();
+    final List<dynamic> initialDisplayNames = initialMemebers.map((d) => d['display']).toList();
+    final multiSelect = MultiSelect(
+      dataSource: dataSource,
+      textField: 'display',
+      valueField: 'value',
+      initialValue: initialValues,
+    );
+    var app = getBasicMultiSelectApp(multiSelect, mockNavigatorObserver, formKey);
+    await tester.pumpWidget(app);
+    Finder multiSelectFinder = find.byType(MultiSelect);
+    await tester.tap(multiSelectFinder);
+    await tester.pumpAndSettle();
+    Finder selectionModalFinder = find.byType(SelectionModal);
+    Finder checkedOptionsFinder = find.descendant(
+        of: selectionModalFinder, matching: find.byIcon(Icons.check_box));
+    expect(checkedOptionsFinder, findsNWidgets(initialValues.length));
+    Finder optionsThatExistFinder = find.byType(Chip);
+    expect(optionsThatExistFinder, findsNWidgets(initialValues.length));
+    Finder selectedOptionsTextFinder = find.descendant(
+        of: optionsThatExistFinder, matching: find.byType(Text));
+    var selectedOptionsText =
+        selectedOptionsTextFinder.evaluate().map((e) => (e.widget as Text).data).toList();
+    expect(selectedOptionsText.every((t) => initialDisplayNames.contains(t)), isTrue);
+  });
+}
+
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
+
+class MyAppToTest extends StatelessWidget {
+  final MultiSelect _multiSelect;
+  final GlobalKey<FormState> _formKey;
+  MyAppToTest(this._multiSelect, this._formKey,);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Something'),
+      ),
+      body: Center(
+        child: new Form(key: _formKey, autovalidate: true, child: _multiSelect),
+      ),
+    );
+  }
 }
