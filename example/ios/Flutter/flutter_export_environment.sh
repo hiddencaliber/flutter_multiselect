042e2ff232dc5c87869415c7716894e1d751773f
hiddencaliber <serendipitous11@gmail.com>
2025-03-01 23:27:02 -0500

    -- changes to support macos (other desktop apps)

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/cerberodev/src/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/cerberodev/GitHub/pub.dev/flutter_multiselect/example"
+export "FLUTTER_ROOT=/Users/vishwajeetrai/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/vishwajeetrai/development/apps/flutter_multiselect/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
